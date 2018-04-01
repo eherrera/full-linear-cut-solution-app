6bae1a0ac52dd9718901375723f3c4c37d82cb18
eherrera <ernestoh2006@gmail.com>
2018-04-01 00:52:26 -0400

    Optimize strategy stable + Adding Unit Test project

---
+++ FullLinearCutSolutionTest/OptimizerTest.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FullLinearCutSolution.Core;
+using FullLinearCutSolution.Core.Model;
+using LinealCutOptimizer.Core.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FullLinearCutSolutionTest
+{
+    [TestClass]
+    public class OptimizerTest
+    {
+        [TestMethod]
+        public void TestOptimizerStrategy()
+        {
+            var result = TestOptimizer(OptimizerStrategy.Optimize);
+            Assert.AreEqual(result.Sum(r => r.BarCount), 31);
+        }
+
+        [TestMethod]
+        public void TestTraditionalStrategy()
+        {
+            var result = TestOptimizer(OptimizerStrategy.Traditional);
+            Assert.AreEqual(result.Sum(r => r.BarCount), 0);
+        }
+
+        private static IEnumerable<CutSolution> TestOptimizer(OptimizerStrategy strategy)
+        {
+            var bar = GetBar();
+            var order = GetOrder();
+            var optimizer = GetOptimizer();
+            var result = optimizer.Optimize(bar, order, strategy);
+            return result;
+        }
+
+        private static Optimizer GetOptimizer()
+        {
+            return new Optimizer();
+        }
+
+        private static Bar GetBar()
+        {
+            return new Bar { Length = 9 };
+        }
+
+        private static Order GetOrder()
+        {
+            return new Order
+            {
+                Items = new List<OrderItem>
+                {
+                    new OrderItem
+                    {
+                        Measurement = 3.98M,
+                        Units = 14
+                    },
+                    new OrderItem
+                    {
+                        Measurement = 3.08M,
+                        Units = 16
+                    },
+                    new OrderItem
+                    {
+                        Measurement = 2.2M,
+                        Units = 16
+                    },
+                    new OrderItem
+                    {
+                        Measurement = 1.5M,
+                        Units = 4
+                    },
+                    new OrderItem
+                    {
+                        Measurement = 1.34M,
+                        Units = 68
+                    },
+                    new OrderItem
+                    {
+                        Measurement = 0.82M,
+                        Units = 28
+                    },
+                    new OrderItem
+                    {
+                        Measurement = 0.25M,
+                        Units = 26
+                    },
+                    new OrderItem
+                    {
+                        Measurement = 0.2M,
+                        Units = 24
+                    },
+                }
+            };
+        }
+    }
+}
---
+++ FullLinearCutSolutionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FullLinearCutSolutionTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FullLinearCutSolutionTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec0e8b10-7e59-472f-9c4f-0276551e9649")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
