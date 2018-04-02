e6e68322087b5e129596cf6dbb12f8fbb4b87cd8
eherrera <ernestoh2006@gmail.com>
2018-04-02 11:46:01 -0400

    Using .net framework 4.0 for xp support

--- full-linear-cut-solution/Properties/Resources.Designer.cs
+++ full-linear-cut-solution/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FullLinearCutSolution.Properties {
+namespace LinealCutOptimizer.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FullLinearCutSolution.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FullLinearCutSolution.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LinealCutOptimizer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- full-linear-cut-solution/Properties/Settings.Designer.cs
+++ full-linear-cut-solution/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FullLinearCutSolution.Properties {
+namespace LinealCutOptimizer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
